d9f07d853a10ee60de79393df44cf8a994aa9210
Nuno Machado <nunom4chado@gmail.com>
2022-07-21 23:10:20 +0100

    adds ex02

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+  while(*str)
+  {
+    ft_putchar(*str);
+    str++;
+  }
+  ft_putchar('\n');
+}
+
+int main(int argc, char **argv)
+{
+  int i;
+
+  i = argc - 1;
+  while (i > 0)
+  {
+    ft_putstr(argv[i]);
+    i--;
+  }
+  return (0);
+}
