d2e82a05d7458cb1c437a113111a7a05197095eb
Nuno Machado <nunom4chado@gmail.com>
2022-07-26 21:30:55 +0100

    adds c05/ex01

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -1,18 +1,11 @@
 int ft_iterative_factorial(int nb)
 {
-  int i;
-  int fact;
-
-  i = 1;
-  fact = 1;
   if (nb < 0)
     return (0);
-  while (i <= nb)
-  {
-    fact *= i;
-    i++;
-  }
-  return (fact);
+  else if (nb == 0)
+    return (1);
+  else
+    return (nb * ft_iterative_factorial(nb - 1));
 }
 
 /*
